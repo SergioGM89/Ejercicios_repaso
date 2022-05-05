fe751eef2ab8b3037ee7fb9cfa642d1a5dd95a4e
Sergio Gil <smontanyana@gmail.com>
2022-05-05 19:45:55 +0200

    tema7 semana1 Ejercicio4 finalizado

--- tema7W1/Ejer4.js
+++ tema7W1/Ejer4.js
@@ -1,126 +1,51 @@
-// const asyncRequest = require(`./asyncRequest`);
+const asyncRequest = require(`./asyncRequest`);
 
-// function contain3(num) {
-//     let numString = num.toString();
-//     for (let i = 0; i < numString.length; i++) {
-//         if (numString.charAt(i) === "3") {
-//             return true;
-//         }
-//     }
-//     return false;
-// }
+let divisible3 = num => (num % 3 === 0);
 
-// function divisible3(num) {
-//     if (num % 3 === 0) {
-//         return true;
-//     }
-//     return false;
-// }
-
-// function fizz(num) {
-//     let randomDelay = Math.round(Math.random() * 1000) + 100;
-//     return new Promise((resolve, reject) => {
-//         setTimeout(function () {
-//             if (contain3(num) || divisible3(num)) {
-//                 return resolve(true);
-//             } else {
-//                 return resolve(false);
-//             }
-//         }, randomDelay);
-//     });
-// }
-
-
-// let finalString = "";
-// for (let num = 1; num <= 300; num++) {
-    
-//     fizz(num)
-//         .then((isFizz) => {
-//             if (isFizz) {
-//                 finalString += "fizz, ";
-//             } else {
-//                 finalString += `${num.toString()}, `;
-//             }
-//             // console.log(finalString);
-//             // return finalString;
-//         });
-
-// }
-// finalString = finalString.substring(0, finalString.length - 2);
-// let datos = new Promise((resolve, reject) => {
-//     resolve(finalString);
-// })
-// datos.then(()=>console.log(datos));
-
-//FUNCIONES ORDENACION RESPUESTAS
-function isUndefined(indice) {
-    if (arrayRespuestas[indice] === undefined) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  
-  function mostrarRecurso(num) {
-    if (arrayRespuestas[num] !== "leido") {
-      console.log(arrayRespuestas[num]);
-      arrayRespuestas[num] = "leido";
-    }
-  }
-  
-  function ordenaResultadosCallbackQue(num, respuesta) {
-    arrayRespuestas[num] = respuesta;
-    for (let i = 1; i <= arrayRespuestas.length; i++) {
-      if (arrayRespuestas[i] === undefined) {
-        return;
-      } else if (arrayRespuestas[i] !== "leido") {   
-        mostrarRecurso(i);
+function fizz(num) {
+  let randomDelay = Math.round(Math.random() * 1000) + 100;
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      if (num.toString().includes("3") || divisible3(num)) {
+        return resolve(true);
       }
-    }
-  }
-  
-  //FUNCIONES COMPROBACION CONDICIONES
-  let contieneNumeroTres = (num) => num.toString().includes("3");
-  let esDivisiblePorTres = (num) => num % 3 == 0;
-  
-  
-  //FUNCION ASÍNCRONA LLAMA A COMPROBACIONES
-  function fizz(num, callback) {
-    const MIN_DELAY = 100;
-    const MAX_DELAY = 1000;
-    let randomDelay = Math.round(Math.random() * MAX_DELAY) + MIN_DELAY;
-    console.log(
-      "***Ejecutando petición de comprobación de condiciones para:" + num + "***"
-    );
-  
-    //Llama a la funcion callback que comprueba las condiciones al cabo de un tiempo
-    setTimeout(() => {
-      callback();
+      return resolve(false);
     }, randomDelay);
+  });
+}
+
+const AMOUNT = 110;
+let finalArray = [];
+
+for (let num = 1; num <= AMOUNT; num++) {
+  fizz(num)
+  .then((boolean) => { 
+    if (boolean) { 
+      finalArray[num] = "fizz";
+    } else { 
+      finalArray[num] = num;
+    } 
+    orderResources(); 
+  });
+}
+
+function orderResources() {
+  for (let i = 1; i < finalArray.length; i++) {
+    if (finalArray[i] !== undefined) {
+      printResource(i);
+    } else {
+      return;
+    }
   }
-  
-  //FUNCION CREACION PROMESA
-  function getResource(num) {
-    return new Promise((resolve, reject) => {
-      try {
-        //fizz(numero, (comprobarCondiciones) => resolve(comprobarCondiciones));
-        fizz(num, () => {
-          if (contieneNumeroTres(num) || esDivisiblePorTres(num)) {
-            return resolve("fizz");
-          } else {
-            return resolve(num);
-          }
-        });
-      } catch (error) {
-        reject("Ha fallado");
-      }
-    });
+}
+
+function printResource(n) {
+  if (!isPrinted(finalArray[n])) {
+    console.log(finalArray[n]);
+    finalArray[n] = "printed";
   }
-  
-  //MAIN
-  let arrayRespuestas = []; //Almacena respuestas leidas
-  for (let numero = 1; numero <= 30; numero++) {
-    getResource(numero).then((dato) => {
-      ordenaResultadosCallbackQue(numero, dato);
-    });
-  }
+}
+
+function isPrinted(resource) {
+  return resource === "printed";
+}
