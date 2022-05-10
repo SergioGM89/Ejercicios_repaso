b610f17cd8329ba38e422baa7bdf6e6b34e4c393
Sergio Gil <smontanyana@gmail.com>
2022-05-10 23:54:44 +0200

    promise.all unificado

--- actividadIntermedia/src/getCompanion.js
+++ actividadIntermedia/src/getCompanion.js
@@ -1,6 +1,21 @@
 import fetch from "node-fetch";
 import { searchCharacter } from "./searchCharacter.js";
 
+async function getCharactersOfEpisode(episode, charactersNotRepeat) {
+
+    await episode.characters.map(async (character) => {
+        console.log(!charactersNotRepeat.has(character));
+        if (!charactersNotRepeat.has(await character)) {
+            charactersNotRepeat.add(await character);
+        }
+    });
+
+    let characters = Array.from(charactersNotRepeat);
+    return await Promise.all(
+        characters.map(async (url) => (await fetch(url)).json())
+    );
+}
+
 async function getCompanions(nameCharacter) {
 
     // CONSULTA PERSONAJE A LA API
@@ -9,37 +24,39 @@ async function getCompanions(nameCharacter) {
     // CONSULTA EPISODIOS
     const urlEpisodes = character.episode;
 
-    const requestsEpisodes = urlEpisodes.map(url => fetch(url));
+    const requestsEpisodes = urlEpisodes.map(async (url) => (await fetch(url)).json());
     const episodes = await Promise.all(requestsEpisodes);
-    const dataEpisodes = episodes.map(episode => episode.json());
-
-    console.log(await dataEpisodes[0]);
+    console.log(episodes);
 
     // GUARDA PERSONAJES
 
     let charactersNotRepeat = new Set();
-    const requestsCharacters = dataEpisodes.map(async (episode) =>
-        await episode.characters.map(url => {
-            if (!charactersNotRepeat.has(url)) {
-                fetch(url);
-                charactersNotRepeat.add(url);
-            }
-        }));
-    const characters = await Promise.all(requestsCharacters);
-    const dataCharacter = characters.map(character => character.json());
+    console.log(episodes[0].id);
+    const requestsCharacters = episodes.map(async (episode) => {
+        let res = await getCharactersOfEpisode(episode, charactersNotRepeat);
+        return res;
+        }
+    );
+    console.log(requestsCharacters);
 
-    console.log(dataCharacter);
+    // const characters = await Promise.all(requestsCharacters);
+    // const dataCharacter = characters.map(character => character.json());
+
+    // console.log(dataCharacter);
 
     // ORDENA PERSONAJES
-    let nameCharacters = dataCharacter.map(character => character.name);
-    nameCharacters.sort();
+    let nameCharacters = [];
+    requestsCharacters.forEach( async (episode) => {
+        nameCharacters = await episode.map(character => character.name);
+        nameCharacters.sort();
+    });
 
     // ELIMINA PERSONAJE ORIGINAL
     let nameCharactersWhithOutOriginal = nameCharacters.filter(nameCharacter => nameCharacter !== character.name);
 
     // MUESTRA PERSONAJES OBTENIDOS
-    
     console.log(nameCharactersWhithOutOriginal);
+    // console.log(nameCharactersWhithOutOriginal);
 
 }
 
